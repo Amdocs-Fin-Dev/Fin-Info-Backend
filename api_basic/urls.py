ff23d083dee577ede2dfc5e092401bb9efd44c39
Alvaro Felix Guzman <okaru.alvaro97@gmail.com>
2021-09-01 16:13:01 -0500

    Added Tecnical Analisis url

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ticker_list, TickerAPIView, TickerDetails, TickerMostrar, TickerMostrarMonth,TickerMostrarWeek
+from .views import ticker_list, TickerAPIView, TickerDetails, TickerMostrar, TickerMostrarMonth,TickerMostrarWeek,TecnicalAnalisis
 #from .views import TickerAPIView, TickerDetails, TickerMostrar
 urlpatterns = [
     path('show/<str:ticker_id>', ticker_list), 
@@ -9,8 +9,8 @@ urlpatterns = [
     # path('mostrar', TickerMostrar.as_view()),
     path('mostrar/w', TickerMostrar.as_view()),
     path('line/w', TickerMostrarWeek.as_view()),
-    path('line/m', TickerMostrarMonth.as_view())
-    
+    path('line/m', TickerMostrarMonth.as_view()),
+    path('analisis/<str:ticker>',TecnicalAnalisis.as_view())
 ]
 
 
