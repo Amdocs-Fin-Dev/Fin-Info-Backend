295eb0cb0b0f916d2e966c43b05e4df3142ff9df
Alvaro Felix Guzman <okaru.alvaro97@gmail.com>
2021-09-06 15:39:50 -0500

    Added mex_stocks models to the admin urls

--- api_basic/admin.py
+++ api_basic/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Ticket
+from .models import Ticket, Mex_stocks
 
 # Register your models here.
 
 admin.site.register(Ticket)
+admin.site.register(Mex_stocks)
 
