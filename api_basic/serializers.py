384e270e0aa358927f386f659de261bcef30f35e
Alvaro Felix Guzman <okaru.alvaro97@gmail.com>
2021-09-06 15:37:23 -0500

    Added mex_stocks serializer

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -1,9 +1,14 @@
 from django.db.models import fields
 from django.db.models.base import Model
 from rest_framework import serializers
-from .models import Ticket
+from .models import Ticket, Mex_stocks
 
 class TickerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = '__all__'
+
+class Mex_stocks_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mex_stocks
+        fields = '__all__' 
