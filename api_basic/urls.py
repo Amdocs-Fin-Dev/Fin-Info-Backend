051c06f722721c59f2751c63c3902f8afa8fa5dd
Alvaro Felix Guzman <okaru.alvaro97@gmail.com>
2021-10-11 01:48:35 -0500

    added new url for comodities endpoint

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import stock_list, ticker_list, TickerAPIView, TickerDetails, TickerMostrar, TickerMostrarMonth,TickerMostrarWeek,TecnicalAnalisis
+from .views import Comodities, stock_list, ticker_list, TickerAPIView, TickerDetails, TickerMostrar, TickerMostrarMonth,TickerMostrarWeek,TecnicalAnalisis
 #from .views import TickerAPIView, TickerDetails, TickerMostrar
 urlpatterns = [
     path('show/<str:ticker_id>', ticker_list), 
@@ -11,7 +11,8 @@ urlpatterns = [
     path('line/w', TickerMostrarWeek.as_view()),
     path('line/m', TickerMostrarMonth.as_view()),
     path('analisis/<str:ticker>/<str:flag>',TecnicalAnalisis.as_view()),
-    path('search/<str:patterMatch>', stock_list)
+    path('search/<str:patterMatch>', stock_list),
+    path('comodities/',Comodities.as_view()),
 ]
 
 
